7fb9227f15b6534f51b441d9cef68c9f33b29ade
Martin Macharia <mosmartin5@gmail.com>
2019-06-12 23:32:59 +0200

    Update app.js
    Add the orders route handler.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 const app = express();
 
-// middleware
+// route handler middleware
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
