8d58b80672c8bab6860ab1edb606208eef74cdc7
Martin Macharia <mosmartin5@gmail.com>
2019-06-24 15:41:15 +0200

    Update products.js
    Update the POST method handler.

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     res.status(200).json({
         message: 'Handling GET requests to /products'
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+
     res.status(201).json({
-        message: 'Handling POST requests to /products'
+        message: 'Product Successfully Created',
+        createdProduct: product
     });
 });
 
-router.get('/:productId', (req, res, next) => {
+router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
 
     res.status(200).json({
@@ -23,7 +29,7 @@ router.get('/:productId', (req, res, next) => {
     });
 });
 
-router.put('/:productId', (req, res, next) => {
+router.put("/:productId", (req, res, next) => {
     const id = req.params.productId;
 
     res.status(200).json({
@@ -32,7 +38,7 @@ router.put('/:productId', (req, res, next) => {
     });
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId;
 
     res.status(200).json({
