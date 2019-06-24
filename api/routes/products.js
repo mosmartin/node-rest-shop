87353d020f80f99185ebe74cec2a63114520c693
Martin Macharia <mosmartin5@gmail.com>
2019-06-24 18:23:15 +0200

    Update products.js
    Update the product route handler.

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("../models/products");
 
 const router = express.Router();
 
@@ -9,14 +11,24 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const product = {
+  // create a product object
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  };
+  });
+
+  // save the product
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
 
   res.status(201).json({
     message: "Product Successfully Created.",
-    product: product
+    data: product
   });
 });
 
