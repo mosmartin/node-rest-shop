a20e888b7bc119716b283e6051b21696c4db7833
Martin Macharia <mosmartin5@gmail.com>
2019-06-24 15:52:37 +0200

    Update products.js
    Format the file.

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,48 +3,48 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+  res.status(200).json({
+    message: "Handling GET requests to /products"
+  });
 });
 
 router.post("/", (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
 
-    res.status(201).json({
-        message: 'Product Successfully Created',
-        createdProduct: product
-    });
+  res.status(201).json({
+    message: "Product Successfully Created.",
+    product: product
+  });
 });
 
 router.get("/:productId", (req, res, next) => {
-    const id = req.params.productId;
+  const id = req.params.productId;
 
-    res.status(200).json({
-        message: 'Success',
-        id: id
-    });
+  res.status(200).json({
+    message: "Success",
+    id: id
+  });
 });
 
 router.put("/:productId", (req, res, next) => {
-    const id = req.params.productId;
+  const id = req.params.productId;
 
-    res.status(200).json({
-        message: 'Successfully Updated',
-        id: id
-    });
+  res.status(200).json({
+    message: "Successfully Updated",
+    id: id
+  });
 });
 
 router.delete("/:productId", (req, res, next) => {
-    const id = req.params.productId;
+  const id = req.params.productId;
 
-    res.status(200).json({
-        message: 'Successfully Deleted',
-        id: id
-    });
+  res.status(200).json({
+    message: "Successfully Deleted",
+    id: id
+  });
 });
 
 module.exports = router;
