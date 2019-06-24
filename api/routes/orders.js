c3c83509ca552fc63c5fb46a8457091302ac2b28
Martin Macharia <mosmartin5@gmail.com>
2019-06-24 15:52:32 +0200

    Update orders.js
    Format the file.

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,41 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /orders'
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to /orders"
+  });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Handling POST requests to /orders'
-    });
+router.post("/", (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  };
+
+  res.status(201).json({
+    message: "Successfully Created an Order.",
+    order: order
+  });
 });
 
-router.get('/:orderId', (req, res, next) => {
-    const id = req.params.orderId;
+router.get("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
 
-    res.status(200).json({
-        message: 'Success',
-        id: id
-    });
+  res.status(200).json({
+    message: "Success",
+    id: id
+  });
 });
 
-router.delete('/:orderId', (req, res, next) => {
-    const id = req.params.orderId;
+router.delete("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
 
-    res.status(200).json({
-        message: 'Success',
-        id: id
-    });
+  res.status(200).json({
+    message: "Success",
+    id: id
+  });
 });
 
 module.exports = router;
