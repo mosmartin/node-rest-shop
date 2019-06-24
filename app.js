68fd91169fc7931b399f7ff6f711f3a67e64a6bd
Martin Macharia <mosmartin5@gmail.com>
2019-06-24 15:52:49 +0200

    Update app.js
    Format the file.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // route handler middleware
@@ -16,18 +16,18 @@ app.use("/orders", orderRoutes);
 
 // handle errors
 app.use((req, res, next) => {
-    const error = new Error("Not Found!");
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found!");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
