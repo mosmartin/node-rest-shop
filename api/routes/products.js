fd3cef69d0a94d8c6e4200cbaa8e1403ca347baa
Martin Macharia <mosmartin5@gmail.com>
2019-06-25 09:20:53 +0200

    Update products.js
    Add the update route handler.

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,7 +4,9 @@ const Product = require("../models/products");
 
 const router = express.Router();
 
-// get all products
+/**
+ * get all products
+ */
 router.get("/", (req, res, next) => {
   Product.find()
     .exec()
@@ -18,7 +20,9 @@ router.get("/", (req, res, next) => {
     });
 });
 
-// create a product
+/**
+ * create a product
+ */
 router.post("/", (req, res, next) => {
   // create a product object
   const product = new Product({
@@ -44,7 +48,9 @@ router.post("/", (req, res, next) => {
     });
 });
 
-// get a product by id
+/**
+ * get a product by id
+ */
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
 
@@ -66,17 +72,32 @@ router.get("/:productId", (req, res, next) => {
     });
 });
 
-// update a product
-router.put("/:productId", (req, res, next) => {
+/**
+ * update a product
+ */
+router.patch("/:productId", (req, res, next) => {
   const id = req.params.productId;
+  const updOps = {};
 
-  res.status(200).json({
-    message: "Successfully Updated",
-    id: id
-  });
+  for (const ops of req.body) {
+    updOps[ops.propertyName] = ops.value;
+  }
+
+  Product.update({ _id: id }, { $set: updOps })
+    .exec()
+    .then(result => {
+      console.log(`\n=== FROM DATABASE ===\n ${result}`);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
-// delete a product
+/**
+ * delete a product
+ */
 router.delete("/:productId", (req, res, next) => {
   const id = req.params.productId;
 
